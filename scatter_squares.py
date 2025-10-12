502354ec87854aa2d302f31aa29f483692aeccef
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-12 15:14:54 -0600

    feat(plot): Save plots automatically

--- scatter_squares.py
+++ scatter_squares.py
@@ -9,6 +9,8 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
+
+# Use a color map.
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
@@ -23,4 +25,5 @@ ax.tick_params(labelsize=14)
 ax.axis([0, 1100, 0, 1_100_000])
 ax.ticklabel_format(style='plain')
 
-plt.show()
+# Save the plot automatically
+plt.savefig('squares_plot.png', bbox_inches='tight')
