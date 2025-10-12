78438535769442ebb62b32fbb1ce35bf0f02163f
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-12 14:31:34 -0600

    feat(plot): Plot and style individual points with scatter()

---
+++ scatter_squares.py
@@ -0,0 +1,11 @@
+"""
+Plotting and Styling individual points with scatter()
+"""
+
+import matplotlib.pyplot as plt
+
+plt.style.use("seaborn-v0_8")
+fig, ax = plt.subplots()
+ax.scatter(2, 4)
+
+plt.show()
