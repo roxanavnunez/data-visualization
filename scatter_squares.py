75bbcbbc9b41aec865f9de528eac9dc450117645
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-12 15:10:36 -0600

    feat(style): Apply a colormap to plot points

--- scatter_squares.py
+++ scatter_squares.py
@@ -9,7 +9,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, color=(0,0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
