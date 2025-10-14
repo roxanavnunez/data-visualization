f91dd36f9260ce6311c1a9973b12ab2422919f08
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-13 22:25:28 -0600

    feat(random-walk): Apply custom styles to the plot

--- rw_visual.py
+++ rw_visual.py
@@ -12,7 +12,12 @@ while True:
     # Plot the points in the walk
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values,rw.y_values,s=15)
+
+    # Color the points
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values,rw.y_values,c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors='none',s=15)
+    
     ax.set_aspect('equal')
     plt.show()
 
