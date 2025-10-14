e80ced253616297477236769b3c8674495a1b068
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-14 16:00:01 -0600

    fix(plot): Update x-axis label

--- chapter15-generating-data/dice_visual.py
+++ chapter15-generating-data/dice_visual.py
@@ -24,4 +24,8 @@ for value in poss_results:
 title = "Results of Rolling Two D6 1,000 Times"
 labels = {"x": "Result", "y": "Frequency of Result"}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+
 fig.show()
