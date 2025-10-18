bfe60d80c3b2baead93123451838285931ca43ee
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-17 18:16:35 -0600

    feat(ch16): Extract and read data

--- chapter16-downloading-data/sitka_highs.py
+++ chapter16-downloading-data/sitka_highs.py
@@ -8,5 +8,10 @@ lines = path.read_text(encoding='utf-8').splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+# Extract high temperatures.
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+print(highs)
