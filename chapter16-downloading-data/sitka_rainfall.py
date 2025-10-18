dc7e59361f008f6ce27768bfde8eca296462b3f8
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-18 17:47:11 -0600

    feat(exercises): Complete Exercise 1 Chapter 16

---
+++ chapter16-downloading-data/sitka_rainfall.py
@@ -0,0 +1,36 @@
+"""
+Chapter 16 Exercise 1
+"""
+from pathlib import Path
+import csv
+from datetime import datetime
+from matplotlib import pyplot as plt
+
+script_dir = Path(__file__).parent
+path = script_dir / 'weather_data/sitka_weather_2021_full.csv'
+lines = path.read_text(encoding='utf-8').splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# Extract dates and PRCP
+dates, prcps = [], []
+for row in reader:
+    current_date = datetime.strptime(row[2],'%Y-%m-%d')
+    prcp = float(row[5])
+    dates.append(current_date)
+    prcps.append(prcp)
+
+# Plot PRCP
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, prcps, color='blue')
+
+# Format plot.
+ax.set_title("Daily Rainfall Amounts, 2021", fontsize=24)
+ax.set_xlabel("", fontsize=14)
+fig.autofmt_xdate()
+ax.set_ylabel("PRCP", fontsize=14)
+ax.tick_params(labelsize=14)
+
+plt.show()
