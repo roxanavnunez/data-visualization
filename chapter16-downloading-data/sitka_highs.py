5855996a388cd0ee7ff403548b93ef53edf57bc7
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-17 18:22:56 -0600

    feat(ch16): Plot data in a temperature chart

--- chapter16-downloading-data/sitka_highs.py
+++ chapter16-downloading-data/sitka_highs.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt
+
 script_dir = Path(__file__).parent
 path = script_dir / 'weather_data/sitka_weather_07-2021_simple.csv'
 lines = path.read_text(encoding='utf-8').splitlines()
@@ -14,4 +16,15 @@ for row in reader:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Format plot.
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
