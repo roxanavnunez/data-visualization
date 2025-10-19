371dd7653391b83dd851ef9363e27465b537e974
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-19 17:46:59 -0600

    feat(ch16): Extract magnitudes

--- chapter16-downloading-data/eq_explore_data.py
+++ chapter16-downloading-data/eq_explore_data.py
@@ -16,4 +16,10 @@ path.write_text(readable_contents)
 
 # Examine all earthquakes in the dataset
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
