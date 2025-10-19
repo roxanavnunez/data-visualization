706f1be53ee3f178d22ecf5baecf1ce0d4c224c0
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-19 17:44:24 -0600

    feat(ch16): Make a list of all earthquakes

--- chapter16-downloading-data/eq_explore_data.py
+++ chapter16-downloading-data/eq_explore_data.py
@@ -7,7 +7,13 @@ path = script_dir / 'eq_data/eq_data_1_day_m1.geojson'
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
+"""
 # Create a more readable version of the data file.
 path = script_dir / 'eq_data/readable_eq_data.json'
 readable_contents = json.dumps(all_eq_data, indent=4)
 path.write_text(readable_contents)
+"""
+
+# Examine all earthquakes in the dataset
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
