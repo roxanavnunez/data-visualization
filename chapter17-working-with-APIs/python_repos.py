720d86fd01b847db903d9aff76f655c7de9db767
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-21 20:33:58 -0600

    feat(ch17): Summarize the Top Repositories

--- chapter17-working-with-APIs/python_repos.py
+++ chapter17-working-with-APIs/python_repos.py
@@ -27,17 +27,17 @@ try:
         repo_dicts = response_dict['items']
         print(f"Repositories returned: {len(repo_dicts)}")
 
-        # Examine the first repository.
-        repo_dict = repo_dicts[0]
-
-        print("\nSelected information about first repository:")
-        print(f"Name: {repo_dict['name']}")
-        print(f"Owner: {repo_dict['owner']['login']}")
-        print(f"Stars: {repo_dict['stargazers_count']}")
-        print(f"Repository: {repo_dict['html_url']}")
-        print(f"Created: {repo_dict['created_at']}")
-        print(f"Updated: {repo_dict['updated_at']}")
-        print(f"Description: {repo_dict['description']}")
+        # Examine the top repositories
+        print(f"\nSelected information about each repository:")
+        for repo_dict in repo_dicts:
+            print("\nSelected information about first repository:")
+            print(f"Name: {repo_dict['name']}")
+            print(f"Owner: {repo_dict['owner']['login']}")
+            print(f"Stars: {repo_dict['stargazers_count']}")
+            print(f"Repository: {repo_dict['html_url']}")
+            print(f"Created: {repo_dict['created_at']}")
+            print(f"Updated: {repo_dict['updated_at']}")
+            print(f"Description: {repo_dict['description']}")
 
 except Exception as e:
     print(f"Error inesperado: {e}")
