7c5ac239c1e9d3612572ad23ce47e2bb30301957
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-22 18:30:40 -0600

    feat(ch17): Add Clickable Links

--- chapter17-working-with-APIs/python_repos_visual.py
+++ chapter17-working-with-APIs/python_repos_visual.py
@@ -24,9 +24,13 @@ try:
 
         # Process repository information.
         repo_dicts = response_dict['items']
-        repo_names, stars, hover_texts = [], [], []
+        stars, hover_texts, repo_links = [], [], []
         for repo_dict in repo_dicts:
-            repo_names.append(repo_dict['name'])
+            # Turn repo names into active links
+            repo_name = repo_dict['name']
+            repo_url = repo_dict['html_url']
+            repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+            repo_links.append(repo_link)
             stars.append(repo_dict['stargazers_count'])
 
             # Build hover texts.
@@ -38,7 +42,7 @@ try:
     # Make visualization
     title = "Most-Starred Python Projects on GitHub"
     labels = {'x': 'Repository', 'y': 'Stars'}
-    fig = px.bar(x=repo_names, y=stars, title=title, labels=labels,
+    fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,
                  hover_name=hover_texts)
 
     fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
