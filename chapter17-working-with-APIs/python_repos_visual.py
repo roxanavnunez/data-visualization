6204be9230b3e6b49e7786bdfa0d4e1654e6b93d
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-22 18:13:23 -0600

    feat(ch17): Style the chart

--- chapter17-working-with-APIs/python_repos_visual.py
+++ chapter17-working-with-APIs/python_repos_visual.py
@@ -30,11 +30,16 @@ try:
             stars.append(repo_dict['stargazers_count'])
     
     # Make visualization
-    fig = px.bar(x=repo_names, y=stars)
+    title = "Most-Starred Python Projects on GitHub"
+    labels = {'x': 'Repository', 'y': 'Stars'}
+    fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+
+    fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
+                      yaxis_title_font_size=20)
     fig.show()
 
 except Exception as e:
-    print(f"Error inesperado: {e}")
+    print(f"Unexpected Error: {e}")
 
 # Delay to avoid saturating the API
 time.sleep(2)
