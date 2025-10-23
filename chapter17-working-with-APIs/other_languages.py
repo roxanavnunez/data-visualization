a58e18de4ced10cb55caa43945d020b222732865
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-22 19:28:07 -0600

    feat(exercises): Complete Exercise 1 Chapter 17

---
+++ chapter17-working-with-APIs/other_languages.py
@@ -0,0 +1,61 @@
+"""
+Chapter 17 Exercise 1
+"""
+import requests
+import time
+import plotly.express as px
+
+# Make an API call and check the response.
+language = 'java'
+url = "https://api.github.com/search/repositories"
+url += f"?q=language:{language}+sort:stars+stars:>10000"
+
+headers ={"Accept": "application/vnd.github.v3+json"}
+
+try:
+    r = requests.get(url,headers=headers)
+    print(f"Status code: {r.status_code}")
+
+    # Process overall results.
+    response_dict = r.json()
+
+    if r.status_code != 200:
+        print(f"Error: {response_dict.get('message','Unknown error')}")
+        if 'rate limit' in response_dict.get('message', '').lower():
+            print("Rate limit exceeded. Wait 1 minute and try again")
+    else:
+        print(f"Complete results: {not response_dict['incomplete_results']}")
+
+        # Process repository information.
+        repo_dicts = response_dict['items']
+        stars, hover_texts, repo_links = [], [], []
+        for repo_dict in repo_dicts:
+            # Turn repo names into active links
+            repo_name = repo_dict['name']
+            repo_url = repo_dict['html_url']
+            repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+            repo_links.append(repo_link)
+            stars.append(repo_dict['stargazers_count'])
+
+            # Build hover texts.
+            owner = repo_dict['owner']['login']
+            description = repo_dict['description']
+            hover_text = f"{owner}<br />{description}"
+            hover_texts.append(hover_text)
+    
+    # Make visualization
+    title = f"Most-Starred {language.title()} Projects on GitHub"
+    labels = {'x': 'Repository', 'y': 'Stars'}
+    fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,
+                 hover_name=hover_texts)
+
+    fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
+                      yaxis_title_font_size=20)
+    fig.update_traces(marker_color='SteelBlue',marker_opacity=0.6)
+    fig.show()
+
+except Exception as e:
+    print(f"Unexpected Error: {e}")
+
+# Delay to avoid saturating the API
+time.sleep(2)
